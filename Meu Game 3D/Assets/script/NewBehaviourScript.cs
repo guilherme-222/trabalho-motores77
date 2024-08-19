42174e98225a6a38eb982abd7d2e17761e5b34ca
guilherme-222 <jesus.g@escolar.ifrn.edu.br>
2024-08-19 16:11:00 -0300

    script do player

---
+++ Meu Game 3D/Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int velocidade = 10;
+    Rigidbody rb;
+ 
+    void Start()
+    {
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+    }
+}
+
+
+
+
+
